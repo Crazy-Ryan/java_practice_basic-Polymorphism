a9f81cde86d01dfdf7352327d28c4e127043936b
Ryan.Wang <ranwang1995@outlook.com>
2020-02-14 02:49:20 +0800

    [WangRan] feat: finish class Application

--- 006_polymorphism/src/main/java/Application.java
+++ 006_polymorphism/src/main/java/Application.java
@@ -1,5 +1,17 @@
 public class Application {
     public static void main(String[] args) {
+        System.out.println("====================");
+        clickF5Respond(new OnClickButton());
+        System.out.println("====================");
+        clickF5Respond(new OnClickButtonInIDEA());
+        System.out.println("====================");
+        clickF5Respond(new OnClickButtonInChrome());
+        System.out.println("====================");
+        clickF5Respond(new OnClickButtonInWechat());
+        System.out.println("====================");
+    }
 
+    public static void clickF5Respond(OnClickButton pressF5) {
+        pressF5.clickF5();
     }
 }
