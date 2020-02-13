feb2bcda6e08a2efb79497b16d7dd5c85eb4cc6a
Ryan.Wang <ranwang1995@outlook.com>
2020-02-14 02:43:13 +0800

    [WangRan] feat: add the behavior when clicking F5 in Wechat

---
+++ 006_polymorphism/src/main/java/OnClickButtonInWechat.java
@@ -0,0 +1,7 @@
+public class OnClickButtonInWechat extends OnClickButton {
+    @Override
+    public void clickF5() {
+        super.clickF5();
+        System.out.println("Under Wechat: the input panel for English pops up");
+    }
+}
