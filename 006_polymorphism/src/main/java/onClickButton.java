9092cb08134c6b914c56de1476d3a3f4fb6d8ee0
Ryan.Wang <ranwang1995@outlook.com>
2020-02-14 02:39:05 +0800

    [WangRan] feat: add the behavior when clicking F5 in IntelliJ IDEA

--- 006_polymorphism/src/main/java/onClickButton.java
+++ 006_polymorphism/src/main/java/onClickButton.java
@@ -1,5 +1,5 @@
 public class onClickButton {
     public void clickF5() {
-        System.out.println("refresh window");
+        System.out.println("Default: refresh");
     }
 }
---
+++ 006_polymorphism/src/main/java/onClickButtonInIDEA.java
@@ -0,0 +1,6 @@
+public class onClickButtonInIDEA extends onClickButton {
+    @Override
+    public void clickF5() {
+        System.out.println("Under IntelliJ IDEA: copy the current window");
+    }
+}
